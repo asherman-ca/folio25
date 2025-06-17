0fc61dec9731eda25af371b027a4932ba06ecd8b
Alex Sherman <asherman.ca@gmail.com>
2025-06-17 12:48:34 -0700

    struct

--- app/data/details.ts
+++ app/data/details.ts
@@ -1,4 +1,144 @@
 export const details = {
-	roles: [],
-	projects: [],
+	roles: [
+		{
+			company: 'Candace Crow Design',
+			link: 'https://candacecrowe.com/',
+			badges: ['Remote'],
+			title: 'Lead Software Engineer',
+			start: '2024',
+			end: null,
+			description:
+				'Ground up development of custom CMS, Database architecture, Prod & Dev infrastructure, Image processing, Partner integrations. Technologies: React, TypeScript, SQL',
+		},
+		{
+			company: 'Lyell Immunopharma',
+			link: 'https://lyell.com/',
+			badges: ['Hybrid'],
+			title: 'Team Leader',
+			start: '2021',
+			end: null,
+			description:
+				'Designed & implemented FE data solution for lyell.com, deployed & maintained IT automation workflows. Technologies: React, TypeScript, Node.js',
+		},
+		{
+			company: 'Nurx',
+			link: 'https://nurx.com',
+			badges: ['On Site'],
+			title: 'Software Engineer',
+			start: '2019',
+			end: '2021',
+			description:
+				'Implimented new features, performed database migrations & mutations, and implimented design patterns. Technologies: Ember, TypeScript, Node.js',
+		},
+		{
+			company: 'MyOnlinePool',
+			link: 'https://myonlinepool.com',
+			badges: ['Remote'],
+			title: 'Lead Software Engineer',
+			start: '2017',
+			end: '2018',
+			description:
+				'Created NFL-fantasy-sports web application. Implimented authentication features, NFL stats integrations and Stripe processing. Technologies: Rails, React, SQL',
+		},
+		{
+			company: 'App Academy',
+			link: 'https://www.appacademy.io/',
+			badges: ['On Site'],
+			title: 'Teaching Assistant',
+			start: '2017',
+			end: '2018',
+			description:
+				'Taught introductory Ruby and Javascript, assisted in platform debugging & feature development, and evaluated applicants. Technologies: Ruby, JavaScript, React, Rails',
+		},
+	],
+	projects: [
+		{
+			title: 'Chonkify',
+			techStack: [
+				'Side Project',
+				'Next.js',
+				'TypeScript',
+				'Node.js',
+				'Postgres',
+			],
+			description:
+				'AI image generator that trains model and generates chonky pets in under 30 seconds',
+			link: {
+				label: 'chonkify.ai',
+				href: 'https://www.chonkify.ai',
+			},
+		},
+		{
+			title: 'Flashdeck',
+			techStack: [
+				'Side Project',
+				'Next.js',
+				'TypeScript',
+				'Node.js',
+				'Postgres',
+			],
+			description: 'AI empowered study platform',
+			link: {
+				label: 'flashdeck-five.vercel.app',
+				href: 'https://flashdeck-five.vercel.app/',
+			},
+		},
+		{
+			title: 'Deep Rock Builds',
+			techStack: [
+				'Side Project',
+				'Next.js',
+				'TypeScript',
+				'Node.js',
+				'Postgres',
+			],
+			description:
+				'Build sharing platform for the Deep Rock Galactic community.',
+			link: {
+				label: 'deeprockbuilds.vercel.app',
+				href: 'https://deeprockbuilds.vercel.app/',
+			},
+		},
+		{
+			title: 'Liquify',
+			techStack: [
+				'Side Project',
+				'Next.js',
+				'TypeScript',
+				'Node.js',
+				'Postgres',
+			],
+			description:
+				'Crypto-futures trading testnet platform with real-time liquification events.',
+			link: {
+				label: 'liquify-three.vercel.app',
+				href: 'https://liquify-three.vercel.app/',
+			},
+		},
+		{
+			title: 'Supper',
+			techStack: ['Side Project', 'TypeScript', 'Next.js', 'Postgres'],
+			description: 'A Job Search Tracker with AI CV Builder',
+			link: {
+				label: 'supper-eta.vercel.app/',
+				href: 'https://supper-eta.vercel.app/',
+			},
+		},
+		{
+			title: 'Chonkify',
+			techStack: [
+				'Side Project',
+				'TypeScript',
+				'Next.js',
+				'Midjourney',
+				'Postgres',
+			],
+			description:
+				'AI image generator that trains model and generates chonky pets in under 60 seconds',
+			link: {
+				label: 'chonkify.vercel.app/',
+				href: 'https://chonkify.vercel.app/',
+			},
+		},
+	],
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,53 @@
-import MaxWidth from './components/MaxWidth'
+import { Link, MailIcon, PhoneIcon } from 'lucide-react'
+import MaxWidth from '@/components/MaxWidth'
+import { GitHubIcon } from './icons/GithubIcon'
+import { LinkedInIcon } from './icons/LinkedinIcon'
+import { Button } from '@/components/ui/button'
+import { details } from './data/details'
 
 export default function Home() {
 	return (
 		<MaxWidth className='justify-between'>
-			<div id='title' className='flex flex-col items-start gap-4'>
-				<h1 className='text-5xl font-bold text-blue-400'>Alex Sherman</h1>
-				<div className='flex flex-col items-start'>
-					<span className='text-blue-400 text-xl font-bold'>
-						Software Engineer
-					</span>
-					<div>contact buttons</div>
+			<div id='title' className='flex flex-col items-start gap-2'>
+				<div className='flex items-end gap-2'>
+					<h1 className='text-5xl font-bold text-blue-700'>Alex Sherman</h1>
+					<span className='text-xl font-bold'>Software Engineer</span>
+				</div>
+				<div className='flex flex-col items-start gap-2'>
+					<div className='flex gap-2'>
+						<Button variant='outline' asChild>
+							<a href='mailto:asherman.ca@gmail.com'>
+								<MailIcon className='w-6 h-6' />
+							</a>
+						</Button>
+						<Button variant='outline' asChild>
+							<a href='tel:4156520317'>
+								<PhoneIcon className='w-6 h-6' />
+							</a>
+						</Button>
+						<Button variant='outline' asChild>
+							<a
+								href='https://www.linkedin.com/in/alex-sherman-1ba03649/'
+								target='_blank'
+							>
+								<LinkedInIcon className='w-6 h-6' />
+							</a>
+						</Button>
+						<Button variant='outline' asChild>
+							<a href='https://github.com/asherman-ca' target='_blank'>
+								<GitHubIcon className='w-6 h-6' />
+							</a>
+						</Button>
+					</div>
 				</div>
 			</div>
 
 			<div className='flex flex-col gap-4'>
-				<div id='roles'>roles</div>
+				<div id='roles'>
+					{details.roles.map((role) => {
+						return <div>{role.company}</div>
+					})}
+				</div>
 
 				<div id='projects'>projects</div>
 			</div>
